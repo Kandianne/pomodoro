522e0b0a95e5012f6d3707c82f68448ab116581e
kandianne <kandipierre@gmail.com>
2015-08-30 17:40:08 -0700

    second update

--- public/javascript/Controllers/homeController.js
+++ public/javascript/Controllers/homeController.js
@@ -1,80 +1,76 @@
-app.controller("homeController", [ "$interval","$timeout" , function($interval, $timeout){
+app.controller("homeController", [ "$interval", function($interval){
 	var vm = this;
 	var interval;
 	vm.page = "Home";
 
+//------Break time--------------------------------------
+vm.breakSecs = 0;
+vm.breakMins = 0;
+vm.breakHrs = 0;
 
+vm.setBreakMins = function(){
+	if(vm.breakSecs === 59){
+		vm.breakMins += 1;
+		vm.breakSecs = 0;
+	}
+	else{vm.breakSecs += 1;}
+};
 
-	/*function timeConstructor(secs, mins, hrs) {
-		this.secs = secs;
-		this.mins = mins;
-		this.hrs = hrs;
-	};
+vm.setBreakHrs = function(){
+	if(vm.breakMins === 59 && vm.breakSecs === 59){
+		vm.breakHrs += 1;
+		vm.breakMins = 0;
+	}
+};
 
-	var zero = new timeConstructor(0, 0, 0);
-	*/
-	vm.secs = 0;
-	vm.mins = 0;
-	vm.hrs = 0;
+vm.startBreak = function(){
+	interval = $interval(vm.setBreakMins, 1000);
+	interval = $interval(vm.setBreakHrs, 1000);
+};
 
+//-------Work time------------------------------------------
 
-	vm.setTime = function(){
-		if(vm.secs === 60){
-			vm.mins += 1;
-			vm.secs = 0;
-		}
-		else{vm.secs += 1;}
+vm.workSecs = 3;
+vm.workMins = 1;
+vm.workHrs = 0;
 
-		if(vm.mins === 60){
-			vm.hrs += 1;
-			vm.mins = 0;
-		}
-		else{vm.mins +=1;}
-	};
+vm.setWorkMins = function(){
+	if(vm.workSecs === 1){
+		vm.workMins -= 1;
+		vm.workSecs = 59;
+	}
+	else {vm.workSecs -=1;}
+};
+
+vm.setWorkHrs = function(){
 	
 
+	/*if(vm.workSecs === 0 && vm.workMins === 0 && vm.workHrs === 0){
+		vm.startBreak();
+	}*/
 
-	vm.tick = function(){
-		interval = $interval(vm.setTime, 1000);
-	};
+
+	if(vm.workMins === 1 && vm.workSecs === 1){
+		vm.workMins = 59;
+		if(vm.workHrs === 0){
+			vm.workHrs = 0;
+		}
+		else{vm.workHrs -= 1};
+		
+	}
+};
+
+
+
+vm.startWork = function(){
+	interval = $interval(vm.setWorkMins, 1000);
+	interval = $interval(vm.setWorkHrs, 1000);
+};
+
 
 }]); 
 
-	//vm.timerAtZeroSecs = 0;
-	//vm.timerAtZeroMins = 0;
-	//vm.timerAtSixty = 60;
 
-//-------------Working----------------	
-
-/*vm.incrementSecs = function() {
-	vm.timerAtZeroSecs += 1;
-};
-vm.incrementMins = function() {
-	vm.timerAtZeroMins += 1;
-};
-
-vm.startSecs = function() {
-	interval = $interval(vm.incrementSecs, 1000);
-};
-
-vm.startMins = function() {
-	timeout = $timeout(vm.incrementMins, 60000);
-	interval = $interval(vm.incrementMins, 60000);
-};
-
-vm.startTime = function() {
-	vm.startSecs();
-	vm.startMins();
-};
-
-//-------------Break----------------	
-vm.decrementTime = function() {
-	vm.timerAtSixty-= 1;
-};
-
-vm.decTime = function(){
-	interval = $interval(vm.decrementTime, 1000)
-};*/
 
 
 
