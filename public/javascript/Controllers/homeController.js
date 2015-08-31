38c5811533dcf3b0dd29629c091feab7ce899920
kandianne <kandipierre@gmail.com>
2015-08-30 21:22:41 -0700

    added userinput & break time 5:30

--- public/javascript/Controllers/homeController.js
+++ public/javascript/Controllers/homeController.js
@@ -5,35 +5,53 @@ app.controller("homeController", [ "$interval", function($interval){
 	vm.page = "Home";
 
 //------Break time--------------------------------------
-vm.breakSecs = 0;
-vm.breakMins = 0;
-vm.breakHrs = 0;
+
 
 vm.setBreakMins = function(){
-	if(vm.breakSecs === 59){
-		vm.breakMins += 1;
-		vm.breakSecs = 0;
+	if(vm.breakSecs === 0){
+		vm.breakSecs = 59;
+
+		if(vm.breakMins === 0 && vm.breakHrs === 0){
+			vm.breakSecs = 0;
+		}
+
+		if(vm.breakMins ===0){
+			vm.breakMins =0;
+		}
+		else
+			{vm.breakMins -= 1;}
+		
 	}
-	else{vm.breakSecs += 1;}
+	else {vm.breakSecs -=1;}
 };
 
 vm.setBreakHrs = function(){
-	if(vm.breakMins === 59 && vm.breakSecs === 59){
-		vm.breakHrs += 1;
-		vm.breakMins = 0;
-	}
+
+	if(vm.breakMins === 1 && vm.breakSecs === 1){
+		
+		if(vm.breakMins === 0 && vm.breakHrs === 0){
+			vm.breakMins = 0;
+		};
+
+		vm.breakMins = 59;
+		if(vm.breakHrs === 0){
+			vm.breakHrs = 0;
+		}
+		else{vm.breakHrs -= 1};
+	};
 };
 
 vm.startBreak = function(){
+	vm.breakSecs = vm.breakingSecs;
+	vm.breakMins = vm.breakingMins;
+	vm.breakHrs = vm.breakingHrs;
 	breakInterval = $interval(vm.setBreakMins, 1000);
 	breakInterval = $interval(vm.setBreakHrs, 1000);
+
 };
 
 //-------Work time------------------------------------------
 
-vm.workSecs = 3;
-vm.workMins = 0;
-vm.workHrs = 0;
 
 vm.setWorkMins = function(){
 	if(vm.workSecs === 0){
@@ -54,10 +72,20 @@ vm.setWorkMins = function(){
 };
 
 vm.setWorkHrs = function(){
-	
+
 	if(vm.workSecs === 0 && vm.workMins === 0 && vm.workHrs === 0){
 		vm.startBreak();
+		vm.breakSecs = 30;
+		vm.breakMins = 5;
+		vm.breakHrs = 0;
+		
 
+	};
+
+	if(vm.workSecs === 0 || vm.workMins === 0 || vm.workHrs === 0){
+		vm.workSecs = 0;
+		vm.workMins = 0;
+		vm.workHrs = 0;
 	};
 
 
@@ -77,6 +105,9 @@ vm.setWorkHrs = function(){
 
 
 vm.startWork = function(){
+	vm.workSecs = vm.workingSecs;
+	vm.workMins = vm.workingMins;
+	vm.workHrs = vm.workingHrs;
 	workInterval = $interval(vm.setWorkMins, 1000);
 	workInterval = $interval(vm.setWorkHrs, 1000);
 };
